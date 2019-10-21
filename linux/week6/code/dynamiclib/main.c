835e24fcedb392048ab796fc0c5482943c8ee209
YOUR NAME <YOUR EMAIL ADDRESS>
2019-10-21 14:11:16 +0800

    change main.c

--- linux/week6/code/dynamiclib/main.c
+++ linux/week6/code/dynamiclib/main.c
@@ -1,9 +1,46 @@
 #include "my.h"
+#include<dlfcn.h>
+#include<stdlib.h>
 int main()
 {
+    void  *handle;
+	int(*f1)();
+	int(*f2)();
+	int(*f3)();
+	char *error;
+
 	int a[5]={323,-77,34,12,56};
-	show(a,5);	
-	printf("max=%d\n",MAX(a,5));
-	printf("sum=%d\n",sum(a,5));
+	handle=dlopen("/home/xiaoke/test4/dynamiclib/libdynamiclib.so",RTLD_LAZY);
+	if(!handle)
+	{
+		perror("load error\n");
+		exit(1);
+	}
+	f1=dlsym(handle,"show");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"error,%s\n",error);
+		exit(1);
+	}
+	f2=dlsym(handle,"MAX");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"error,%s\n",error);
+		exit(1);
+	}
+	f3=dlsym(handle,"sum");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"error,%s\n",error);
+		exit(1);
+	}
+	
+	f1(a,5);
+	printf("max=%d",f2(a,5));
+	printf("sum=%d",f3(a,5));
+	if(dlclose(handle)<0){
+		fprintf(stderr,"error,%s\n",error);
+		exit(1);
+	}
 	return 0;
 }
