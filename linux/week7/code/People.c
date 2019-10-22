300f0fbb5914778f4091b7f43cdc6698b6133e42
YOUR NAME <YOUR EMAIL ADDRESS>
2019-10-22 16:43:18 +0800

    add a People program

---
+++ linux/week7/code/People.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+int globa=4;
+int main(void)
+{
+	pid_t pid;
+	int vari=5;
+	printf("before fork\n");
+	if((pid=fork())<0)
+	{
+		printf("fork error\n");
+		exit(0);
+	}
+	else if(pid==0)
+	{
+		globa++;
+		vari--;
+		printf("Child changed\n");
+		printf("Child :globa=%d vari=%d\n",globa,vari);
+	}
+	else
+	{
+		printf(" parent didn't change\n");
+		printf("parent :globa=%d vari=%d\n",globa,vari);
+		exit(0);
+	}
+
+}
